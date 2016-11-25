e5554da3dc1071a9c01be4641be49d42f837ba4a
smith02620 <swith02620@ey.gov.tw>
2016-11-25 17:13:13 +0800

    完成OPEN API

--- src/app/what-we-do/detail/detail.component.ts
+++ src/app/what-we-do/detail/detail.component.ts
@@ -8,69 +8,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
-item2;
 
-posts = [];
+  mappingTable = {
+    "open-api": "52",
+    "civic-participation": "53"
+  };
 
-tid;
-  constructor(private router:Router, private route: ActivatedRoute, private datadetail: DataService) {
-    this.tid = '52';
-    datadetail.getData(this.tid)
-    .subscribe((value) => 
-    {
-    this.item2 = JSON.parse(value.text());
-    console.log(this.item2);
-    
-    var tmp = this.item2.post_stream.posts;
-    var count1=0;
-    for(var i in tmp){
-      var tmp2 = {};
-      tmp2['content'] = tmp[i]['cooked'].split("<hr>");
-      count1+=1;
-      //console.log(count1);
-      this.posts.push(tmp2);
-    
-    }
+  item2;
+  item1;
+  posts = [];
+  posts2 = [];
+  dataservice;
 
-    // this.posts = [
-    //   {"content":["what is open api","how we might ....."]},
-    //   {"content":["what is open api","how we might ....."]},
-    //   {"content":["what is open api"]},
-    //   {"content":["what is open api","how we might ....."]}
-    // ];
-    
+  tid;
 
-    }
-    );
-  }
-
-/*getData(i: number) {
-
-   *ngIf="item2"
-      <div *ngFor="let post of item2.post_stream.posts; let i = index">
-        <div *ngIf="i==1" [innerHTML]="post.cooked"></div>
-      </div>
-    </div>
-    //console.log('https://talk.pdis.nat.gov.tw/t/' + id +'.json');
-    // console.log('http://dt.iii.org.tw/t/' + id +'.json');
-    // return this.http.get('https://talk.pdis.nat.gov.tw/t/17.json')
-    // return this.http.get('https://talk.pdis.nat.gov.tw/t/8.json')
-    // http://dt.iii.org.tw/t/17.json
-    //return this.httpData.get('https://talk.pdis.nat.gov.tw/t/' + id +'.json')
-    // return this.httpData.get('http://dt.iii.org.tw/t/' + id +'.json')
-    // return this.httpData.get('http://192.168.99.1/t/' + id +'.json')
-    // return this.httpData.get('/assets/' + id +'.json')
-    ;
-  }*/
-
-
-  id: String;
+  id: string;
   params;
-  ngOnInit() {
+
+  constructor(private router: Router, private route: ActivatedRoute, private datadetail: DataService) {
+    //this.tid = '52';
+    this.dataservice = datadetail;
     this.params = this.route.params;
-    this.route.params.subscribe(params =>{
+    this.route.params.subscribe(params => {
       this.id = params['id'];
     });
+  }
+  // this.posts = [
+  //   {"content":["what is open api","how we might ....."]},
+  //   {"content":["what is open api","how we might ....."]},
+  //   {"content":["what is open api"]},
+  //   {"content":["what is open api","how we might ....."]}
+  // ];
+
+
+  /*getData(i: number) {
+  
+     *ngIf="item2"
+        <div *ngFor="let post of item2.post_stream.posts; let i = index">
+          <div *ngIf="i==1" [innerHTML]="post.cooked"></div>
+        </div>
+      </div>
+      //console.log('https://talk.pdis.nat.gov.tw/t/' + id +'.json');
+      // console.log('http://dt.iii.org.tw/t/' + id +'.json');
+      // return this.http.get('https://talk.pdis.nat.gov.tw/t/17.json')
+      // return this.http.get('https://talk.pdis.nat.gov.tw/t/8.json')
+      // http://dt.iii.org.tw/t/17.json
+      //return this.httpData.get('https://talk.pdis.nat.gov.tw/t/' + id +'.json')
+      // return this.httpData.get('http://dt.iii.org.tw/t/' + id +'.json')
+      // return this.httpData.get('http://192.168.99.1/t/' + id +'.json')
+      // return this.httpData.get('/assets/' + id +'.json')
+      ;
+    }*/
+
+
+  ngOnInit() {
+    this.datadetail.getData(this.mappingTable[this.id])
+      .subscribe((value) => {
+        this.item2 = JSON.parse(value.text());
+        console.log(this.item2);
+
+        var tmp = this.item2.post_stream.posts;
+        var count1 = 0;
+        for (var i in tmp) {
+          var tmp2 = {};
+          tmp2['content'] = tmp[i]['cooked'].split("<hr>");
+          this.posts.push(tmp2);
+        }
+      });
   }
 
   ngOnDestroy() {
