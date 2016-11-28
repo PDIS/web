5af4af4676e256379cc4112a3ac1c52d486ca6d2
john5601 <john560148@gmail.com>
2016-11-28 10:29:22 +0800

    getData2

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class DataService {
@@ -15,16 +16,16 @@ export class DataService {
   }
 
   getData(id: string) {
-    console.log('https://talk.pdis.nat.gov.tw/t/' + id +'.json');
+    console.log('https://talk.pdis.nat.gov.tw/t/' + id + '.json');
     // console.log('http://dt.iii.org.tw/t/' + id +'.json');
     // return this.http.get('https://talk.pdis.nat.gov.tw/t/17.json')
     // return this.http.get('https://talk.pdis.nat.gov.tw/t/8.json')
     // http://dt.iii.org.tw/t/17.json
-    return this.httpData.get('https://talk.pdis.nat.gov.tw/t/' + id +'.json')
-    // return this.httpData.get('http://dt.iii.org.tw/t/' + id +'.json')
-    // return this.httpData.get('http://192.168.99.1/t/' + id +'.json')
-    // return this.httpData.get('/assets/' + id +'.json')
-    ;
+    return this.httpData.get('https://talk.pdis.nat.gov.tw/t/' + id + '.json')
+      // return this.httpData.get('http://dt.iii.org.tw/t/' + id +'.json')
+      // return this.httpData.get('http://192.168.99.1/t/' + id +'.json')
+      // return this.httpData.get('/assets/' + id +'.json')
+      ;
   }
 
   getList() {
@@ -32,8 +33,33 @@ export class DataService {
     // http://dt.iii.org.tw/c/pdis.json
     // https://talk.pdis.nat.gov.tw/t/how-we-work-tools/54.json
     return this.httpList.get('https://talk.pdis.nat.gov.tw/t/how-we-work-tools/54.json')
-    // return this.httpList.get('/assets/pdis.json')
-    ;
+      // return this.httpList.get('/assets/pdis.json')
+      ;
+  }
+
+  getData2(category: string) {
+
+    var topics: Observable<any>;
+    var posts = [];
+    console.log('https://talk.pdis.nat.gov.tw/c/' + category + '.json');
+    topics
+    this.httpData.get('https://talk.pdis.nat.gov.tw/c/' + category + '.json').subscribe((value) => {
+      topics = JSON.parse(value.text());
+
+      console.log(topics['topic_list']['topics']);
+      for (var i in topics['topic_list']['topics']) {
+        this.getData(topics[i]).subscribe((value) => { posts.push(value.text()) });
+      }
+    });;
+
+
+    console.log(posts);
+
+    // return this.httpData.get('https://talk.pdis.nat.gov.tw/c/pdis-site' + id +'.json')
+    // return this.httpData.get('http://dt.iii.org.tw/t/' + id +'.json')
+    // return this.httpData.get('http://192.168.99.1/t/' + id +'.json')
+    // return this.httpData.get('/assets/' + id +'.json')
+    return posts;
   }
 
   // toggleTopic1() {
--- src/app/how-we-work/track/track.component.ts
+++ src/app/how-we-work/track/track.component.ts
@@ -1,3 +1,5 @@
+import { DataService } from './../../data-service.service';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackComponent implements OnInit {
 
-  constructor() { }
+item2;
 
+posts = [];
+
+tid;
+
+  constructor(private router:Router, private route: ActivatedRoute, private datadetail: DataService) { 
+    this.tid = '76';
+    
+  }
+
+  id: String;
+  params;
   ngOnInit() {
+    // this.params = this.route.params;
+    // this.route.params.subscribe(params =>{
+    //   this.id = params['id'];
+    // });
+    
+    console.log(this.datadetail.getData2("pdis-site/how-we-work-track"));
   }
 
 }
