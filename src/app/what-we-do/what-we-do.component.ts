92e3c02f96421d27ea4d4f6a6e44db61cbe66635
mark19891107 <mark19891107@gmail.com>
2016-12-14 16:45:05 +0800

    what we do api

--- src/app/what-we-do/what-we-do.component.ts
+++ src/app/what-we-do/what-we-do.component.ts
@@ -1,3 +1,5 @@
+import { Http } from '@angular/http';
+import { DataService } from './../shared/dataService/data-service.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhatWeDoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService, private http:Http) { }
+
+  private topics = [];
+
+  private getIds() {
+    return this.http.get("https://talk.pdis.nat.gov.tw/c/pdis-site/what-we-do.json")
+      .map(function (data) {
+        data = data.json();
+        var ids = [];
+        var topics = data['topic_list']['topics'];
+        topics.forEach(function (topic) {
+          ids.push(topic['id']);
+        });
+        return ids;
+      })
+      // .do(data => console.log(data));
+  }
+
+  private getPostCooked(id: string) {
+    return this.http.get("https://talk.pdis.nat.gov.tw/t/" + id + ".json")
+      .map(function (data) {
+        data = data.json();
+        return data['post_stream']['posts'][0]['cooked'];
+      })
+      // .do(data => console.log(data));
+  }
 
   ngOnInit() {
+    this.getIds()
+    .subscribe(ids => {
+      ids.forEach(id => {
+        this.getPostCooked(id)
+        .do(data => console.log(data))
+        .subscribe(postCooked=>{
+          this.topics.push(postCooked);
+        })
+      })
+    });
   }
 
 }
