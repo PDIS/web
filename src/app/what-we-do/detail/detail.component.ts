3d7dedf67f37225a60e6069097267a67f3c1bf18
smith <swith02620@ey.gov.tw>
2016-12-20 16:00:19 +0800

    嵌入影片問題修正

--- src/app/what-we-do/detail/detail.component.ts
+++ src/app/what-we-do/detail/detail.component.ts
@@ -9,55 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-
-  
-  item2;
-  posts = [];
-  posts2 = [];
-  dataservice;
-  youtube1: HTMLCollectionOf<Element>;
-  id: string;
-  params;
-  dangerousVideoUrl;
-  videoUrl=[];
-  youtubecount;
-  constructor(private router: Router, private route: ActivatedRoute, private datadetail: DataService,private sanitizer: DomSanitizer) {
-    this.dataservice = datadetail;
-    this.params = this.route.params;
-    this.route.params.subscribe(params => {
-    this.id = params['id'];
-    }); 
-  }
  
+  get_article;
+  posts = [];
+  page_id: string;
+  edit_youtube;
+  constructor(private router: Router, private route: ActivatedRoute, private datadetail: DataService,private sanitizer: DomSanitizer) {
+    this.route.params.subscribe(params => {
+    this.page_id = params['id'];
+    }); 
+  } 
   ngOnInit() {
-    this.datadetail.getData(this.id)
+    this.datadetail.getData(this.page_id)
     .subscribe((value) => {
-      this.item2 = JSON.parse(value.text());
-      var tmp = this.item2.post_stream.posts; 
-      for (var i in tmp) {
-        var tmp2 = {};
-        tmp2['content'] = tmp[i]['cooked'].split("<hr>");
-        this.youtubecount=tmp2;
-        console.log(tmp2['content']);    
-        this.youtube();      
-        this.posts.push(this.youtubecount); 
-      }
+      this.get_article = JSON.parse(value.text()); /***get_article***/
+      var article = this.get_article.post_stream.posts; 
+      for (var i in article) {
+        var article_sort = {};
+        article_sort['content'] = article[i]['cooked'].split("<hr>"); /***split <hr>***/
+        this.edit_youtube=article_sort;           
+        this.youtube();         
+        this.posts.push(this.edit_youtube); /***Send to the web page***/
+      }     
     });
   }
   youtube(){
     var dom = new DOMParser();
-    for(var j=0; j<2; j++){
-      var  doc = dom.parseFromString(this.youtubecount['content'][j],"text/html");    
-      this.youtube1 = doc.getElementsByClassName("lazyYT");                 
-      if(this.youtube1.length != 0 ){
-        this.updateVideoUrl(this.youtube1[0]['dataset'].youtubeId);
-        this.youtubecount['content'][5]="you-tube";        
+    var search_youtube: HTMLCollectionOf<Element>;
+    var regular_Expression_youtube= /<[a-z]{3}\s[a-z]{5}="\w{6}"\s[a-z \-]{15}.*<\/[a-z]{3}>/;
+    for(var i=0; i<2; i++){
+      var  doc = dom.parseFromString(this.edit_youtube['content'][i],"text/html");   //***Convert String format to HTML***/
+      search_youtube = doc.getElementsByClassName("lazyYT");                     
+      if(search_youtube.length != 0 ){
+        for(var k =0; k< search_youtube.length;k++){
+          var iframe_youtube="<iframe src=https://www.youtube.com/embed/"+search_youtube[k]['dataset'].youtubeId+"><\/iframe>";          
+          this.edit_youtube['content'][i]=this.edit_youtube['content'][i].replace(regular_Expression_youtube,iframe_youtube);  /***replace <div class=lazyYT... to <iframe...***/
+        }      
+      this.edit_youtube['content'][i]=this.sanitizer.bypassSecurityTrustHtml(this.edit_youtube['content'][i]); /***Sanitization HTML***/
       }               
     }
-  }
-  updateVideoUrl(id: string) {
-    this.dangerousVideoUrl = 'https://www.youtube.com/embed/' + id;
-    this.videoUrl[0] = this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
-  } 
+  }  
   ngOnDestroy() { }
 }
+
