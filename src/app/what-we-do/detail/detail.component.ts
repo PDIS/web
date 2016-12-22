edc3b0b9f48823ddcd26193c5c04abed045e7fda
smith <swith02620@ey.gov.tw>
2016-12-22 16:45:23 +0800

    修正

--- src/app/what-we-do/detail/detail.component.ts
+++ src/app/what-we-do/detail/detail.component.ts
@@ -12,14 +12,10 @@ import { Discourselink} from './../../discourselink';
 })
 export class DetailComponent implements OnInit {
  
-  // get_article;
   posts = [];
   page_id: string;
-  // edit_youtube;
-
-  constructor(private router: Router, private route: ActivatedRoute, private datadetail: DataService,private sanitizer: DomSanitizer,private Dlink: Discourselink) {
-  // link;
-  // constructor(private router: Router, private route: ActivatedRoute, private datadetail: DataService,private sanitizer: DomSanitizer) {
+ 
+  constructor(private router: Router, private route: ActivatedRoute, private datadetail: DataService,private sanitizer: DomSanitizer) {
     this.route.params.subscribe(params => {
     this.page_id = params['id'];
     }); 
@@ -42,10 +38,6 @@ export class DetailComponent implements OnInit {
 
       });
     });
-    
-    console.log(Discourselink.host);
-    
-    
   }
 
   lazyTY2iframe(article){
