666ba2023a23b9c4477ae2f3df44b475a179b68e
jokeum <joke0111@gmail.com>
2016-12-22 15:36:05 +0800

    update tools & detail page

--- src/app/what-we-do/detail/detail.component.ts
+++ src/app/what-we-do/detail/detail.component.ts
@@ -10,45 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
  
-  get_article;
+  // get_article;
   posts = [];
   page_id: string;
-  edit_youtube;
+  // edit_youtube;
+
   constructor(private router: Router, private route: ActivatedRoute, private datadetail: DataService,private sanitizer: DomSanitizer) {
     this.route.params.subscribe(params => {
     this.page_id = params['id'];
     }); 
   } 
+
   ngOnInit() {
     this.datadetail.getData(this.page_id)
     .subscribe((value) => {
-      this.get_article = JSON.parse(value.text()); /***get_article***/
-      var article = this.get_article.post_stream.posts; 
-      for (var i in article) {
-        var article_sort = {};
-        article_sort['content'] = article[i]['cooked'].split("<hr>"); /***split <hr>***/
-        this.edit_youtube=article_sort;           
-        this.youtube();         
-        this.posts.push(this.edit_youtube); /***Send to the web page***/
-      }     
+
+      /***get_article***/
+      var articles = JSON.parse(value.text()).post_stream.posts;
+
+      articles.forEach(element => {
+        /***split an article by <hr>***/
+        var article = element['cooked'].split("<hr>"); 
+        article = this.lazyTY2iframe(article);
+        this.posts.push(article);
+
+      });
     });
   }
-  youtube(){
+
+  lazyTY2iframe(article){
     var dom = new DOMParser();
-    var search_youtube: HTMLCollectionOf<Element>;
-    var regular_Expression_youtube= /<[a-z]{3}\s[a-z]{5}="\w{6}"\s[a-z \-]{15}.*<\/[a-z]{3}>/;
-    for(var i=0; i<2; i++){
-      var  doc = dom.parseFromString(this.edit_youtube['content'][i],"text/html");   //***Convert String format to HTML***/
-      search_youtube = doc.getElementsByClassName("lazyYT");                     
-      if(search_youtube.length != 0 ){
-        for(var k =0; k< search_youtube.length;k++){
-          var iframe_youtube="<iframe src=https://www.youtube.com/embed/"+search_youtube[k]['dataset'].youtubeId+"><\/iframe>";          
-          this.edit_youtube['content'][i]=this.edit_youtube['content'][i].replace(regular_Expression_youtube,iframe_youtube);  /***replace <div class=lazyYT... to <iframe...***/
-        }      
-      this.edit_youtube['content'][i]=this.sanitizer.bypassSecurityTrustHtml(this.edit_youtube['content'][i]); /***Sanitization HTML***/
-      }               
+    var nodes: HTMLCollectionOf<Element>;
+    var regexp_youtube= /<div class="lazyYT".*<\/div>/;
+
+    for (var i = 0; i < article.length; i++) {
+      var e = article[i];
+      var iframe_string;
+      var doc = dom.parseFromString(e, "text/html");
+      nodes = doc.getElementsByClassName("lazyYT");
+
+      for (var j = 0; j < nodes.length; j++) {
+        iframe_string = "<iframe src=https://www.youtube.com/embed/"+nodes[j]['dataset'].youtubeId+" width="+nodes[j]['dataset'].width+" height="+nodes[j]['dataset'].height+"><\/iframe>";          
+      }
+      /***replace <div class=lazyYT... to <iframe...***/  
+      e = e.replace(regexp_youtube,iframe_string);  
+      /***Sanitization HTML***/
+      e = this.sanitizer.bypassSecurityTrustHtml(e);  
+      article[i] = e;
     }
+        
+    return article;
   }  
+
   ngOnDestroy() { }
+
 }
 
