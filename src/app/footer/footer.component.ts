a4fc7e9022b9eabf3213f63b786236df5faf701e
jokeum <joke0111@gmail.com>
2017-02-02 16:43:23 +0800

    improve footer's icon style

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -10,6 +10,15 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	/* let each listed external link append with an icon */
+  	let list = Array.from(document.querySelectorAll("li a[target='_blank']"));
+  	for(let e of list) {
+		let icon = document.createElement("i");
+		icon.className = "fa fa-external-link";
+		icon.style.paddingLeft = "1ex";
+  		e.appendChild(icon);
+  	}
+
   }
 
 }
