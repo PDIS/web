073e70654e30bab074f991f698760446f5e09738
mark19891107 <mark19891107@gmail.com>
2017-03-13 14:15:55 +0800

    add pipe to replace amp; closed #36

---
+++ src/app/shared/replacePipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'replacePipe'
+})
+export class replacePipe implements PipeTransform {
+
+  transform(target: string, args: any[]): any {
+     console.log(target.replace(/&amp;/g,'&'))
+    return target.replace(/&amp;/g,'&')
+  }
+
+}
