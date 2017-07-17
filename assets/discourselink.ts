ea6226882b2adcf9438d3459062849d46b0e8a72
XanxusX <tako30215@hotmail.com>
2017-07-17 17:50:57 +0800

    Updates

--- assets/discourselink.ts
+++ assets/discourselink.ts
@@ -10,7 +10,7 @@ export class Discourselink {
     static Filename = ".json"
     static tmp = "about-the-how-we-work-track-tmp-category";
     static getId ="how-we-work-track-tmp";
-    static GroupUrl = "https://talk.pdis.nat.gov.tw/groups/PDIS/members.json"
+    static GroupUrl = "https://talk.pdis.nat.gov.tw/groups/PDIS/members.json?limit=100000"
     static UserUrl = "https://talk.pdis.nat.gov.tw/u/"
     static InfoUrl = "https://talk.pdis.nat.gov.tw/c/wiselike/profile-"
     // static WhoweareUrl = "https://talk.pdis.nat.gov.tw/c/pdis-site/who-we-are.json"
