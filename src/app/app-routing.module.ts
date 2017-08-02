03f84be24b1e850100a987a723422a9f87eabd12
jokeum <joke0111@gmail.com>
2017-08-02 17:41:20 +0800

    add menu item to coincide

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,25 +6,26 @@ import { HowWeWorkComponent } from './how-we-work/how-we-work.component';
 import { WhatWeDoComponent } from './what-we-do/what-we-do.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WorkComponent } from "app/what-we-do/work/work.component";
-import { WorklistComponent } from "app/what-we-do/worklist/worklist.component";
+import { WorkComponent } from "./what-we-do/work/work.component";
+import { WorklistComponent } from "./what-we-do/worklist/worklist.component";
 import { WhoWeAreComponent } from "./who-we-are/who-we-are.component";
+// import { NotFoundComponent } from "./404/404.component";
 
 const routes: Routes = [
-  { path: '', redirectTo:'what-we-do', pathMatch:'full' },
   { path: 'what-we-do/worklist', component: WorklistComponent },
-  // { path: 'what-we-do/work', component: WorkComponent },
+  { path: 'what-we-do/beliefs', component: WhatWeDoComponent },
   { path: 'what-we-do/:id', component: DetailComponent },
   { path: 'what-we-do', component: WhatWeDoComponent },
   { path: 'how-we-work/tools', component: ToolsComponent },
   { path: 'how-we-work/tracks', component: TracksComponent },
   { path: 'how-we-work', component: HowWeWorkComponent },
   { path: 'get-involved', component: GetInvolvedComponent },
-  { path: 'who-we-are', component: WhoWeAreComponent }
+  { path: 'who-we-are', component: WhoWeAreComponent },
+  { path: '', redirectTo:'what-we-do', pathMatch:'full' },
+  // { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes, { useHash: true })],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: []
